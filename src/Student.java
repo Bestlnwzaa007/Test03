b1d4746072e0e0c8a81704e65f83575720201fab
wawaexd <chinnabutk68@nu.ac.th>
2026-02-12 00:51:48 +0700

    Student ถูกแก้กลับเป็นหมือนเดิมแล้วจ้า

--- src/Student.java
+++ src/Student.java
@@ -10,7 +10,7 @@ public class Student {
     }
 
     private String calculateGrade(int score) {
-        if (score >= 100) return "A";
+        if (score >= 80) return "A";
         else if (score >= 70) return "B";
         else if (score >= 60) return "C";
         else if (score >= 50) return "D";
