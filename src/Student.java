39dc5b55399ba04b2681319b7a7348fa8f7f4b90
somchatxa007 <somchatc68@nu.ac.th>
2026-02-11 20:28:22 +0700

    แก้ Student

--- src/Student.java
+++ src/Student.java
@@ -8,6 +8,7 @@ public class Student {
         System.out.println("Name: "+ name);
         System.out.println("GTOA (maraq obama): "+ gtaV);
         System.out.println("Gu ma Law Nong");
+        System.out.println("Gu Pen Gay Nika niaka");
     }
 
     public boolean isExcellent() {
