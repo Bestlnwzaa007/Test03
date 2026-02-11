7f82d1626ac5558991f585bbe5d51d39756c2605
kongthapr68 <kongthapr68@nu.ac.th>
2026-02-11 20:07:41 +0700

    แก้ Studernt

--- src/Student.java
+++ src/Student.java
@@ -7,6 +7,7 @@ public class Student {
         System.out.println("ID: "+ id);
         System.out.println("Name: "+ name);
         System.out.println("GTOA (maraq obama): "+ gtaV);
+        System.out.println("Gu ma Law Nong");
     }
 
     public boolean isExcellent() {
